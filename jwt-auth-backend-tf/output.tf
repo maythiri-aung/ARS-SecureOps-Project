3a8d7ac4b6fcdc9d7c9285d4e69292033712d093
PONE <user@Pone-Mac.local>
2024-09-24 18:44:36 +0900

    update

--- jwt-auth-backend-tf/output.tf
+++ jwt-auth-backend-tf/output.tf
@@ -1,9 +1,4 @@
 output "role_name" {
-  description = "Name of the role"
-  value       = vault_jwt_auth_backend_role.admin-role.role_name
-}
-
-output "openid_claims" {
-  description = "OpenID Claims for trust relationship"
-  value       = vault_jwt_auth_backend_role.admin-role.bound_claims
+  description = "Name of the auth role all vcs /modules"
+  value       = vault_jwt_auth_backend_role.jwt_admin_role.role_name
 }
