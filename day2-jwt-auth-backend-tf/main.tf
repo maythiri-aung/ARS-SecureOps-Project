66689605bf2f4179263812f910479124b3c4507a
PONE <user@Pone-Mac.local>
2024-10-17 15:37:30 +0900

    update

--- day2-jwt-auth-backend-tf/main.tf
+++ day2-jwt-auth-backend-tf/main.tf
@@ -65,7 +65,7 @@ EOT
 
 resource "vault_jwt_auth_backend_role" "jwt_auth_role" {
   backend        = vault_jwt_auth_backend.jwt_auth.path
-  role_name      = "admin-role"
+  role_name      = var.role_name
   token_policies = [vault_policy.admin_policy.name]
 
   bound_audiences   = ["vault.workload.identity"]
